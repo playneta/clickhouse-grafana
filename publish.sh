20f4b9c0770cce4f37cc6b1ea34897d0e4e6db93
southernBuryatian <sasha.faleychik@gmail.com>
2019-05-28 18:01:06 +0300

    publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 
-ARCHIVE="playneta-clickhouse-grafana-1.8.1.zip"
+ARCHIVE="playneta-clickhouse-grafana-1.8.4.zip"
 URL="https://nexus-sonatype.playneta.gg/nexus/content/sites/grafana"
 
-zip -r /tmp/${ARCHIVE} app/dist
+rm -rf dist
+mkdir dist
+cp -R app/dist/* dist
+zip -r /tmp/${ARCHIVE} dist
 curl --user admin:admin123 \
      --upload-file /tmp/${ARCHIVE} ${URL}/${ARCHIVE}
